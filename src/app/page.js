c03347b6c74c8c35a2f75838940e3a124baec995
Viddhi Ladva <vidhiladva@gmail.com>
2024-06-20 22:33:09 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className=" font-extrabold text-9xl">
-      <h1>Viddhi Ladva</h1>
+      <h1>ViddhiLadva</h1>
     </main>
   );
 }
